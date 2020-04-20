388c937e7d83bbc476d5ece613eeafd4c3a32c77
Luke <lellis20@punahou.edu>
2020-04-20 03:56:44 +0000

    Added StringConverter method headings

---
+++ StringConverter.java
@@ -0,0 +1,25 @@
+// Luke Ellis String Converter
+public class StringConverter 
+{
+    
+    public static String reverse(String str)
+    {
+        
+    }
+    
+    public static boolean checkPalindrome(String str)
+    {
+        
+    }
+    
+    public static String pigLatinate(String str)
+    {
+        
+    }
+    
+    public static String shorthand(String str)
+    {
+        
+    }
+    
+}
